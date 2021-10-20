8a125e1f2f3ad7be572f31ee368f3dbb2f6bd92b
Andres2D <andres2d1997.1206@gmail.com>
2021-10-20 18:54:24 -0500

    Auth user on chat view

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,33 @@
 
-const socket = io();
+let user = null;
+let socket = null;
+const url = 'http://localhost:8080/api/auth';
+
+const validateJWT = async() => {
+    const token = localStorage.getItem('token') || '';
+
+    if(token.length <= 10) {
+        window.location = 'index.html';
+        throw new Error('Invalid token');
+    }
+
+    const resp = await fetch(url, {
+        headers: {'x-token': token}
+    });
+
+    const {user: userDB, token: tokenDB} = await resp.json();
+    localStorage.setItem('token', tokenDB);
+    user = userDB;
+    console.log(userDB, tokenDB);
+}
+
+const main = async() => {
+
+    // Validate JWT
+    await validateJWT();
+
+}
+
+main();
+
+// const socket = io();
